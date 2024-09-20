08db52beca8331b0136570849a3f6654933716cd
eunhee <heeday3001@gmail.com>
2024-09-20 22:55:52 +0900

    board
    board

---
+++ boardserver/src/main/java/com/fastcampus/boardserver/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.fastcampus.boardserver.controller;
+
+//import com.fastcampus.boardserver.aop.LoginCheck;
+import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.dto.request.UserDeleteId;
+import com.fastcampus.boardserver.dto.request.UserLoginRequest;
+import com.fastcampus.boardserver.dto.request.UserUpdatePasswordRequest;
+import com.fastcampus.boardserver.dto.response.LoginResponse;
+import com.fastcampus.boardserver.dto.response.UserInfoResponse;
+import com.fastcampus.boardserver.service.impl.UserServiceImpl;
+import com.fastcampus.boardserver.utils.SessionUtil;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/users")
+@Log4j2
+public class UserController {
+
+    private final UserServiceImpl userService;
+    private static final ResponseEntity<LoginResponse> FAIL_RESPONSE = new ResponseEntity<LoginResponse>(HttpStatus.BAD_REQUEST);
+
+    @Autowired
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("sign-up")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void signUp(@RequestBody UserDTO userDTO) {
+        log.info("signUp");
+        if (UserDTO.hasNullDataBeforeSignup(userDTO)) {
+            throw new NullPointerException("회원가입시 필수 데이터를 모두 입력해야 합니다.");
+        }
+        userService.register(userDTO);
+    }
+
+}
