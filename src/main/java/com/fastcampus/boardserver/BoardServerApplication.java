8a10ddf2ad0e51b88164e89bc08acce2df1d9ab7
heedayday <heeday3001@gmail.com>
2024-09-23 22:17:00 +0900

    setting
    setting

--- src/main/java/com/fastcampus/boardserver/BoardServerApplication.java
+++ src/main/java/com/fastcampus/boardserver/BoardServerApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BoardServerApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(BoardServerApplication.class, args);
+
+
+		
 	}
 
 }
