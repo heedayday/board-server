c43b09f8311bfe638b42417e73786c0e1e8fb822
heedayday <heeday3001@gmail.com>
2024-11-15 22:49:43 +0900

    사용자 정의 예외처리
    사용자 정의 예외처리

--- src/main/java/com/daybyday/postingBoard/exception/DuplicateIdException.java
+++ src/main/java/com/daybyday/postingBoard/exception/DuplicateIdException.java
@@ -1,7 +1,9 @@
 package com.daybyday.postingBoard.exception;
 
-public class DuplicateIdException extends RuntimeException { 
+public class DuplicateIdException extends RuntimeException {
+  //사용자 정의 예외 이때 extends RuntimeException 가 포인트
   public DuplicateIdException(String msg) {
     super(msg);
   }
+
 }
